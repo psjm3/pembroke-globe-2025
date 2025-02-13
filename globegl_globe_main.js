f01cbe0f1f1956e660f4d1ce6dc6ba4000402062
psjm3 <psjm3@cam.ac.uk>
2025-02-13 15:34:18 +0000

    Updated camera position and profile data display

--- globegl_globe_main.js
+++ globegl_globe_main.js
@@ -3,20 +3,28 @@ import Globe from "./globegl.js";
 
 const myGlobe = Globe();
 
+// Title Pembroke Women: A World of Impact
+
 myGlobe(document.getElementById('app'))
     .globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
     .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
     .backgroundImageUrl('//unpkg.com/three-globe/example/img/night-sky.png');
 
 myGlobe.controls().autoRotate = true;
-myGlobe.controls().autoRotateSpeed = 1.0;
+myGlobe.controls().autoRotateSpeed = 0.8;
+
+var camToSave = {};
+camToSave.position = myGlobe.camera().position.clone();
+camToSave.rotation = myGlobe.camera().rotation.clone();
 
 function showProfiles() {
     stopDistribution();
-    myGlobe.controls().autoRotate = true;
-    fetch('./data/pembroke_alumni.arr')
-    .then(response => 
-        response.json().then(json => {
+    myGlobe.camera().position.set(camToSave.position.x, camToSave.position.y, camToSave.position.z);
+    myGlobe.controls().autoRotate = false;
+
+    fetch('./data/pembroke_selected_alumni.arr')
+    .then(response => response.json()
+    .then(json => {
             console.log(json);
             myGlobe
                 .htmlElementsData(json)
@@ -32,51 +40,55 @@ function showProfiles() {
                     el.appendChild(img);
                     el.style['pointer-events'] = 'auto';
                     el.style.cursor = 'pointer';
-                    el.onclick= (e) => setToFocus(e, d.imageUrl, d.NAME, d.facts, d.lat, d.lng, d.altitude);
+                     el.onclick= (e) => setToFocus(e, d);
                     return el;
                 })
         })
     );
-    fetch('../data/pembroke_alumni.geojson')
-        .then(res => res.json())
-        .then(countries => {
-            myGlobe
-                .labelsData(countries.features)
-                .labelLat(d => d.properties.latitude)
-                .labelLng(d => d.properties.longitude)
-                .labelText(d => d.properties.shortname)
-                .labelColor(() => 'rgb(255, 255, 0)')
-                .labelSize(1)
-                .labelResolution(10)
-                .labelDotRadius(0);
-            setTimeout(() => myGlobe
-                .labelsTransitionDuration(4000)
-                .labelAltitude(
-                    d =>
-                        Math.max(0.05, Math.sqrt(+d.properties.labelrank)*0.05))
-                // .htmlTransitionDuration(4000)
-                // .htmlAltitude(d => Math.max(0.05, Math.sqrt(+d.altitude)*0.05))
-            , 2000);
-        });
+    // fetch('../data/pembroke_alumni.geojson')
+    //     .then(res => res.json())
+    //     .then(countries => {
+    //         myGlobe
+    //             .labelsData(countries.features)
+    //             .labelLat(d => d.properties.latitude)
+    //             .labelLng(d => d.properties.longitude)
+    //             //.labelText(d => d.properties.shortname)
+    //             .labelColor(() => 'rgb(255, 255, 0)')
+    //             .labelSize(1)
+    //             .labelResolution(10)
+    //             .labelDotRadius(0);
+    //         setTimeout(() => myGlobe
+    //             .labelsTransitionDuration(4000)
+    //             .labelAltitude(
+    //                 d =>
+    //                     Math.max(0.05, Math.sqrt(+d.properties.labelrank)*0.05))
+    //             // .htmlTransitionDuration(4000)
+    //             // .htmlAltitude(d => Math.max(0.05, Math.sqrt(+d.altitude)*0.05))
+    //         , 2000);
+    //     });
 }
 
-function setToFocus(e, imageURL, name, facts, lat, lng, altitude) {
+function setToFocus(e, data) {
     stopProfiles();
     myGlobe
-        .pointOfView({ lat: lat, lng: lng, altitude: altitude }, 0)
+        .pointOfView({ lat: data.lat, lng: data.lng, altitude: data.altitude }, 0)
         .htmlElementsData([{
-            "name":name,
-            "imageURL":imageURL,
-            "facts":facts,
-            "lat":lat,
-            "lng":lng,
-            "altitude":altitude
+            "name":data.name,
+            "imageUrl":data.imageUrl,
+            "lat":data.lat,
+            "lng":data.lng,
+            "altitude":data.altitude,
+            "matriculation_year":data.matriculation_year,
+            "city_region":data.city_region,
+            "country": data.country,
+            "job": data.job,
+            "organisation": data.organisation
         }])
         .htmlElement(thisData => {
             let popup = document.createElement('div');
             popup.setAttribute('class', 'card');
             let img = document.createElement('img');
-            img.setAttribute('src', thisData.imageURL);
+            img.setAttribute('src', thisData.imageUrl);
             img.setAttribute('alt', thisData.name);
             img.setAttribute('width', "20%");
             img.setAttribute('height', "20%");
@@ -84,12 +96,15 @@ function setToFocus(e, imageURL, name, facts, lat, lng, altitude) {
             container.setAttribute('class', 'container');
             container.innerHTML = `
                 <h2>${thisData.name}</h2>
-                <p class="profileText">${thisData.facts}</p>`;
+                <p class="profileText">Matriculation Year: ${thisData.matriculation_year}
+                <br>Country: ${thisData.country}
+                <br>Job: ${thisData.job}
+                <br>Organisation: ${thisData.organisation}</p>
+                `;
             popup.appendChild(img);
             popup.appendChild(container);
             return popup;
         });
-    toggleRotation();
 }
 
 function stopProfiles() {
@@ -98,8 +113,9 @@ function stopProfiles() {
 
 function showDistribution() {
     stopProfiles();
+    myGlobe.controls().autoRotate = true;
 
-    fetch('./data/pembroke_alumni.arr')
+    fetch('./data/All_pembroke_women.arr')
     .then(response => 
         response.json().then(json => {
             myGlobe.htmlElementsData(json)})
@@ -136,14 +152,6 @@ function toggleView() {
         stopDistribution();
         showHighlights();
         viewButton.value = 'View - Distribution';
-        // const imgElement = document.getElementById("thumbnail");
-        // //imgElement.addEventListener('click', (event) => setToFocus(event));
-        // imgElement.addEventListener('click', () => {
-        //     console.log("img clicked: ", img.target);
-        // });
-        // imgElement.lat = d.latitude;
-        // imgElement.lng = d.longitude;
-        // imgElement.altitude = d.altitude;
     } else {
         stopProfiles();
         showDistribution();
